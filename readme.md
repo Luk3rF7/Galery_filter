5acc492a36e0ee638d88e6bf37b16cffdeea4747
Lucas Felipe <118012621+Luk3rF7@users.noreply.github.com>
2023-08-02 17:02:59 -0300

    chore:filtragem de imagem

---
+++ readme.md
@@ -0,0 +1,29 @@
+<h1 align="center"> Filtro de Galeria</h1>
+ <h3>Projeto contem base e fundamento da junção dos 3 tecnologia junto com front end </h3>
+ <hr>
+ <a href="https://luk3rf7.github.io/filtro_Galeria/"> Filtro Galeria link </a>
+</section>
+<hr>
+<section style="font-size:18px;">
+<h3>Tecnologia utilizada:</h3>
+
+<li>Bootstrap 5
+<li>Webpack 5
+<li> Html
+<li> Sass
+<li> javascript
+<li> jquery
+<section style="font-size:18px;">
+
+</section>
+<br>
+<section style="font-size:18px;">
+ <h3>Dev dependencies </h3>
+ <hr>
+<img src="./img/dependecie.png">
+
+<h3>Config do Webpack </h3>
+<p>configurações basica e utilização de otimização do webpack, webserver
+<img src="./img/configwebpack.png">
+</section>
+
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const path = require('path');
+const webpack = require('webpack')
+// unificar arquivos
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+  mode: modoDev ? 'development' : 'production',
+  entry:'./src/index.js', //ponto de entrada
+  output: {
+      filename: 'app.js', // nomedo arquivo raiz
+      path: __dirname + '/doc',  // caminho onde sera jogado arquivo
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/doc'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  optimization:{
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+            ecma: 6,
+        },
+    }),
+    new CssMinimizerPlugin({})
+    ]
+  },
+      // area de plugin
+  plugins: [
+      // função construtora recebe obj com parametros
+      new MiniCssExtractPlugin({
+        // recebe nome do arq q quero criar e gerado
+         filename: './css/style.css',  // proriedade
+        
+      }),
+      new CopyWebpackPlugin({
+        patterns:[
+          {context:'src/',from: '**/*.html'},
+          {context:'src/',from: 'imgs/**/*'}
+        ]
+      })
+  ],
+  module:{   // regras - loader
+    rules: [
+      {   // regras
+        test:/\.(sa|sc|c)ss$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+    },{
+      test:/\.(png|svg|jpg|gif)$/,
+      use:['file-loader']
+    },{
+      test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+      use:['file-loader']
+    } 
+  ]
+  }
+}
