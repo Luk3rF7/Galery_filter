f7d00bc47d493966ccebf87f68dbfaefad7993f3
Lucas Felipe <118012621+Luk3rF7@users.noreply.github.com>
2023-08-02 17:03:26 -0300

    feat: initi

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// arquivo SASS 
+
+import './css/index.scss'
+
+// importar dependencia
+import'jquery'
+import'bootstrap'
+
+//meu arq js
+
+import './js/core/includes'
+import './js/plugin/cityButtons'
